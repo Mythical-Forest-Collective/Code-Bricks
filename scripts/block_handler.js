74197e9984723200d5b965ba7eea91054e72ae0d
Event Horizon Collective <random.access.exomemory@gmail.com>
2022-11-27 21:22:56 +0000

    Current work, for some reason innerHTML isn't working.

--- scripts/block_handler.js
+++ scripts/block_handler.js
@@ -5,19 +5,74 @@ if (jQuery.browser.mobile) {
   $('head').append('<script src="scripts/dragndroppolyfill.js"></script>');
 }
 
-var bricks = [];
+var validChars = 'abcdefghijklmnopqrstuvwxyz';
+var validCharsLen = validChars.length;
+
+function makeId() {
+  var result = '';
+  for ( var i = 0; i < 5; i++ ) {
+      result += validChars.charAt(Math.floor(Math.random() * validCharsLen));
+  }
+  return result;
+}
+
+var walls = [];
+var bricks = {};
+
+function uniqueIdGen() {
+  var unid = makeId();
+
+  var uniqueId = false;
+  while (!uniqueId) {
+    if (!(unid in walls.map(w => w.id)) || !(bricks.keys().contains(unid))) {
+      uniqueId = true;
+    } else {
+      unid = makeId();
+    }
+  }
+
+  return unid;
+}
 
 // It creates a wall of bricks, that allow for easy separation and joining of bricks into one group
-function createBrick(data = {"name": "example_name"}) {
+function createBrick(data = {version:1,name:"python::builtin::print",arguments:{text:"string"},generated_output:"print(<text>)",displayed:["print", "[text]"],head:-1,foot:-1,innerbrickcount:0,innerbricks:[]}) {
+  const wallId = uniqueIdGen();
+
   var brick = document.createElement('div');
+  var wall = {version: 1, id: wallId, bricks: []};
+
+  const brickId = uniqueIdGen();
+
+  brick.id = brickId;
+
+  wall.bricks.push(brickId);
+  data.id = brickId
+  bricks[brickId] = data;
+
+  walls.push(wall);
+
   brick.draggable = true;
-  brick.innerText = data.name;
+
   brick.classList.add("brick");
   brick.classList.add("laidbrick");
 
-  bricks.push(brick);
-
   $('#bricklayer').append(brick);
+
+  var toDisplay = "";
+  for (var tx in data.displayed) {
+    if (tx.startsWith('[') && tx.endsWith(']')) {
+      if (data.arguments.contains(tx.substr(1, str.length-1))) {
+        toDisplay += "<div class=\"variable\"></div>";
+      } else {
+        toDisplay += tx;
+      }
+      if (data.displayed.indexOf(tx) != data.displayed.length) {
+        toDisplay += ' ';
+      }
+    }
+  }
+
+  brick.innerHTML = toDisplay;
 }
 
 //#region Event Handling
@@ -41,8 +96,8 @@ addEventListener('dragend', (event) => {
 
 addEventListener('keydown', (event) => {
   if (event.key == "q" && !event.repeat) {
-    console.log("Make a new block");
-    createBrick({"name": "readable text"})
+    console.log("Made a new block");
+    createBrick()
   }
 });
 //#endregion
